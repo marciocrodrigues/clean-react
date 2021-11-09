6761d2caf81ee7648f00400a260372a340396dd5
DESKTOP-POURV4F\marcio <marciodeath@hotmail.com>
2021-11-08 23:14:20 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    colllectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}'
-    ],
-    coverageDirectory: 'covarage',
-    testEnvironment: 'node',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'covarage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
 }
